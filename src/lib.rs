a866994be88c13dcaff50474d45e767496ac3739
Niel Drummond <niel@drummond.lu>
2024-01-20 08:54:30 +0000

    test(docs): Add doc test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,61 @@
+//! [![crates.io](https://img.shields.io/crates/v/hyper-named-pipe.svg)](https://crates.io/crates/bollard)
+//! [![license](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+//! [![appveyor](https://ci.appveyor.com/api/projects/status/n5khebyfae0u1sbv/branch/master?svg=true)](https://ci.appveyor.com/project/fussybeaver/boondock)
+//! [![docs](https://docs.rs/bollard/badge.svg)](https://docs.rs/hyper-named-pipe/)
+//!
+//! # Hyper-named-pipe: Hyper client bindings for Windows Named Pipes
+//!
+//! Exposes a HTTP interface over Tokio's Named Pipes implementation through a Hyper Connection
+//! interface.
+//!
+//! # Install
+//!
+//! Add the following to your `Cargo.toml` file
+//!
+//! ```nocompile
+//! [dependencies]
+//! hyper-named-pipe = "*"
+//! ```
+//!
+//! # Usage
+//!
+//! Ensure host's are hex-encoded when passed into `HyperUri` as this will bypass validation.
+//!
+//! ```rust,no_run
+//! # use http_body_util::{BodyExt, Full};
+//! # use hyper::body::Bytes;
+//! # use hyper_named_pipe::{NamedPipeConnector, NAMED_PIPE_SCHEME};
+//! # use hyper_util::{
+//! #     client::legacy::Client,
+//! #     rt::{TokioExecutor, TokioIo},
+//! # };
+//! let pipe_name = r"\\.\pipe\named-pipe";
+//!
+//! let builder = Client::builder(TokioExecutor::new());
+//! let client: Client<NamedPipeConnector, Full<Bytes>> = builder.build(NamedPipeConnector);
+//!
+//! let host = hex::encode(pipe_name);
+//! let uri_str = format!("{}://{}", NAMED_PIPE_SCHEME, host);
+//! let url = uri_str.parse().expect("Invalid URI");
+//!
+//! async move {
+//!   client.get(url).await.expect("Unable to fetch URL with Hyper client");
+//! };
+//! ```
+//!
+//!
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![warn(rust_2018_idioms)]
+
 use hex::FromHex;
 use hyper::rt::ReadBufCursor;
 use hyper_util::client::legacy::connect::{Connected, Connection};
@@ -17,9 +75,11 @@ use std::time::Duration;
 
 use winapi::shared::winerror;
 
+/// TODO
 pub const NAMED_PIPE_SCHEME: &str = "net.pipe";
 
 pin_project! {
+    /// TODO
     pub struct NamedPipeStream {
         #[pin]
         io: NamedPipeClient,
@@ -27,6 +87,7 @@ pin_project! {
 }
 
 impl NamedPipeStream {
+    /// TODO
     pub async fn connect<A>(addr: A) -> Result<NamedPipeStream, io::Error>
     where
         A: AsRef<Path> + AsRef<OsStr>,
@@ -85,6 +146,7 @@ impl hyper::rt::Write for NamedPipeStream {
 }
 
 #[derive(Clone, Copy, Debug)]
+/// TODO
 pub struct NamedPipeConnector;
 
 impl tower_service::Service<hyper::Uri> for NamedPipeConnector {
